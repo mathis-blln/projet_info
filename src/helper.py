bd2fbd02edaf90020bb090fd947b9bc1c5f2f562
Mathis <mathis.bouillon@eleve.ensai.fr>
2023-11-21 11:50:51 +0100

    f

--- src/helper.py
+++ src/helper.py
@@ -17,3 +17,17 @@ def element_distinct(liste: list):
             elements_distincts.append(element)
 
     print(elements_distincts)
+
+
+def split_input(user_input):
+    # Diviser la chaîne en une liste en utilisant la virgule comme séparateur
+    elements_list = [element.strip() for element in user_input.split(",")]
+    return elements_list
+
+
+# Demander à l'utilisateur d'entrer une chaîne
+user_input = input("Entrez une chaîne d'éléments séparés par des virgules : ")
+
+# Appeler la fonction avec l'entrée utilisateur et afficher le résultat
+result = split_input(user_input)
+print(result)
