b6732b79acaad4dd1cfe7583becb14f6a1d2813c
Mathis <mathis.bouillon@eleve.ensai.fr>
2023-11-24 14:44:03 +0100

    m

--- src/Classe/PrixCarburant.py
+++ src/Classe/PrixCarburant.py
@@ -1,5 +1,6 @@
 from Classe.TypeCarburant import TypeCarburant
 
+
 class PrixCarburant:
     def __init__(self, id_carburant, id_station, prix):
         """
@@ -10,15 +11,14 @@ class PrixCarburant:
             prix (float): Le prix du carburant en unité monétaire (par exemple, en euros par litre).
         """
         self.id_carburant = id_carburant
-        self.id_station = id_station 
+        self.id_station = id_station
         self.prix = prix
 
-        def __str__(self):
-        return f"ID carburant: {self._id_utilisateur}, coûtant : {self.prix} dans la station d'id : {self.id_station}"
-
-         """
+    def __str__(self):
+        """
         Retourne une représentation sous forme de chaîne de caractères de l'objet Utilisateur.
 
         Returns:
             str: Une chaîne de caractères représentant l'ID de l'utilisateur et son mot de passe.
-        """     
+        """
+        return f"ID carburant: {self._id_utilisateur}, coûtant : {self.prix} dans la station d'id : {self.id_station}"
