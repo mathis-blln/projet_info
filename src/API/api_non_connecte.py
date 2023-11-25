e0ee3f5b0058179d9c7d4bc49ac267e63a71f1a3
Quentin <id2315@DOMENSAI.ECOLE>
2023-11-25 23:09:37 +0100

    chapo

--- src/API/api_non_connecte.py
+++ src/API/api_non_connecte.py
@@ -16,14 +16,18 @@ from Session import Session
 app2 = FastAPI()
 
 
-@app2.get("/distincts/elements/carburants&services/")
+@app2.get(
+    "/distincts/elements/carburants&services/",
+    description="Obtenir tous les services et carburants distincts dans le fichier XML",
+)
 async def get_distinct_information():
     station = StationsService()
     return station.get_distinct_elements()
 
 
 @app2.get(
-    "/recherche/par/filtres/adresse/{n}/{services_recherches}/{carburant_recherche}/{adresse_utilisateur}"
+    "/recherche/par/filtres/adresse/{n}/{services_recherches}/{carburant_recherche}/{adresse_utilisateur}",
+    description="Obtenir toutes les stations correspondant à un filtre choisi utilisant l'adresse",  # noqa: E501
 )
 async def obtenir_informations_station_par_adresse(
     n: int, services_recherches: str, carburant_recherche: str, adresse_utilisateur: str
@@ -36,7 +40,8 @@ async def obtenir_informations_station_par_adresse(
 
 
 @app2.get(
-    "/recherche/par/filtres/{n}/{services_recherches}/{carburant_recherche}/{latitude}/{longitude}"
+    "/recherche/par/filtres/{n}/{services_recherches}/{carburant_recherche}/{latitude}/{longitude}",
+    description="Obtenir toutes les stations correspondant à un filtre choisi utilisant les coordonnées latitude et longitude",  # noqa: E501
 )
 async def obtenir_informations_station(
     n: int,
