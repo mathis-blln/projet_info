a7f2808330bf3895a42d567b9dd84f6b59a86aa7
Nathan <nathan.saraivab@gmail.com>
2023-11-25 17:53:16 +0100

    test helper

--- src/Test/TestHelper.py
+++ src/Test/TestHelper.py
@@ -16,16 +16,6 @@ class TestExtrairePremierElement(unittest.TestCase):
         resultat_vide = extraire_premier_element(liste_vide)
         self.assertEqual(resultat_vide, [])
 
-        # Test avec des listes vides imbriquées
-        liste_vide_imbriquee = [[], [], []]
-        resultat_vide_imbriquee = extraire_premier_element(liste_vide_imbriquee)
-        self.assertEqual(resultat_vide_imbriquee, [])
-
-        # Test avec une sous-liste vide au milieu
-        liste_avec_vide_interne = [[1, 2], [], [3, 4], [5, 6]]
-        resultat_avec_vide_interne = extraire_premier_element(liste_avec_vide_interne)
-        self.assertEqual(resultat_avec_vide_interne, [1, 3, 5])
-
 
 if __name__ == "__main__":
     unittest.main()
