f69fc04fedab530ddd955f2cbc0c1ebec603535b
stephane <stephane.xavier@eleve.ensai.fr>
2023-11-25 19:10:07 +0100

    recherche simple

--- src/view/start_view.py
+++ src/view/start_view.py
@@ -2,6 +2,7 @@ from InquirerPy import prompt
 from view.connexion_view import ConnexionView
 from view.inscription_view import InscriptionView
 from view.menu_view import MenuView
+from API.api_non_connecte import app2
 
 
 class StartView:
@@ -26,11 +27,7 @@ class StartView:
                     ],
                 }
             )
-
-            if response["choix"] == "Quitter":
-                break
-
-            elif response["choix"] == "Connexion":
+            if response["choix"] == "Connexion":
                 auth_result = self.connexion_view.make_choice()
 
                 if auth_result is None:  # Si l'authentification réussit
@@ -53,4 +50,43 @@ class StartView:
                     ):  # Si l'authentification réussit après l'inscription
                         self.menu_view.display_info()
                         self.menu_view.make_choice()
-            return None
+
+            elif response["choix"] == "Faire une recherche":
+                import uvicorn
+                import subprocess
+
+                def lancer_api2():
+                    subprocess.Popen(
+                        [
+                            "uvicorn",
+                            "API.api_non_connecte:app2",
+                            "--host",
+                            "127.0.0.1",
+                            "--port",
+                            "8000",
+                        ]
+                    )
+
+                lancer_api2()
+
+                print("Ouvrez le lien suivant")
+                print("http://127.0.0.1:8000/docs")
+                response_back = prompt(
+                    {
+                        "type": "list",
+                        "name": "choix_back",
+                        "message": "Que voulez-vous faire?",
+                        "choices": [
+                            "Retour au menu",
+                            "Quitter",
+                        ],
+                    }
+                )
+
+                if response_back["choix_back"] == "Retour au menu":
+                    continue  # Redémarrer la boucle pour afficher à nouveau les options du menu
+
+                if response_back["choix_back"] == "Quitter":
+                    break
+
+        return None
