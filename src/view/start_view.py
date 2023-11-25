ca5326d16b362acd5797c4c3999685914909f20d
stephane <stephane.xavier@eleve.ensai.fr>
2023-11-25 16:17:37 +0100

    API dans view

--- src/view/start_view.py
+++ src/view/start_view.py
@@ -3,6 +3,28 @@ from view.connexion_view import ConnexionView
 from view.inscription_view import InscriptionView
 from view.menu_view import MenuView  # Importez votre MenuView appropriée
 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict
+from DAO.listeDAO import ListeDAO
+from typing import List as PyList
+from Services.consulterListeFavoris import ConsulterListesFavoris
+from Services.service_station import StationsService
+from typing import Optional  # Importez Optional
+from Services.modif_stations_de_listes import ModifStationListes
+import json
+from fastapi.responses import JSONResponse
+import datetime
+import subprocess
+
+
+def lancer_api():
+    # Lancer l'API FastAPI avec uvicorn
+    subprocess.Popen(["uvicorn", "app:app", "--host", "127.0.0.1", "--port", "80"])
+
+
+lancer_api()
+
 
 class StartView:
     def __init__(self):
