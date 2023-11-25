bd5cebf5bd53f78c4e4071a989e8a5c93fb17f5d
stephane <stephane.xavier@eleve.ensai.fr>
2023-11-25 12:16:38 +0100

    menu

--- src/view/menu_view.py
+++ src/view/menu_view.py
@@ -1,52 +1,34 @@
 import requests
 from view.abstract_view import AbstractView
-from view.session_view import Session
-from DAO.inscriptionDAO import InscriptionDAO
+from InquirerPy import inquirer
 
 
 class MenuView(AbstractView):
-    def __init__(self):
-        self.__questions = [
-            {
-                "type": "list",
-                "name": "choice",
-                "message": "Que voulez-vous faire ?",
-                "choices": [
-                    "Effectuer une recherche",
-                    "Consulter mes stations favorites",
-                    "Modifier/Supprimer mes stations favorites",
-                    "Déconnexion",
-                    # {"name": "Effectuer une recherche", "value": 1},
-                    # {"name": "Consulter mes stations favorites", "value": 2},
-                    # {"name": "Modifier/Supprimer mes stations favorites", "value": 3},
-                    # {"name": "Créer une liste de stations favorites", "value": 4},
-                    # {"name": "Déconnexion", "value": 5},
-                ],
-            },
-        ]
-
     def display_info(self):
         print("Que voulez-vous faire ?")
 
     def make_choice(self):
+        choices = [
+            {"name": "Effectuer une recherche", "value": "search"},
+            {"name": "Consulter mes stations favorites", "value": "favorites"},
+            {
+                "name": "Modifier/Supprimer mes stations favorites",
+                "value": "modify_delete",
+            },
+            {"name": "Déconnexion", "value": "disconnect"},
+        ]
+
         while True:
-            answers = prompt(self.__questions)
-            choice = answers["choice"]
+            choice = inquirer.select(
+                message="Que voulez-vous faire ?", choices=choices
+            ).execute()
 
-            if choice == "Effectuer une recherche":
-                # Appel à l'API pour obtenir toutes les listes favorites
-                response = requests.get("http://127.0.0.1/listesFav/")
-                if response.status_code == 200:
-                    data = response.json()
-                    print("Listes favorites disponibles:")
-                    for key, value in data.items():
-                        print(f"{key}: {value}")
-                else:
-                    print("Erreur lors de la récupération des listes favorites.")
+            if choice == "search":
+                # Code pour effectuer une recherche
+                pass
 
-            elif choice == "Consulter mes stations favorites":
-                # Appel à l'API pour obtenir une liste spécifique
-                numero = int(input("Entrez le numéro de la liste: "))
+            elif choice == "favorites":
+                numero = int(input("Entrez le numéro de la liste : "))
                 response = requests.get(f"http://127.0.0.1/listesFav/{numero}")
                 if response.status_code == 200:
                     data = response.json()
@@ -54,15 +36,11 @@ class MenuView(AbstractView):
                 else:
                     print(f"Erreur lors de la récupération de la liste {numero}.")
 
-            elif choice == "Modifier/Supprimer mes stations favorites":
-                # Ajoutez le code pour l'option "Modifier/Supprimer mes stations favorites"
+            elif choice == "modify_delete":
+                # Code pour modifier ou supprimer les stations favorites
                 pass
 
-            # elif choice == "4":
-            #     pass
-
-            elif choice == "Déconnexion":
-                # Session().clear_session()  # Efface les informations de session
+            elif choice == "disconnect":
                 print("Déconnexion réussie.")
                 input("Appuyez sur Entrée pour quitter...")
                 return None
